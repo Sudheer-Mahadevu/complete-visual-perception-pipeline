ad393b262b9f8bf6bab3c7cecf7c4249db9041ed
Mahadevu Sudheer <mahadevusudheer274@gmail.com>
2026-04-03 16:56:51 +0530

    add vg11-encoder and classification functionality

--- models/classification.py
+++ models/classification.py
@@ -3,6 +3,8 @@
 
 import torch
 import torch.nn as nn
+from models.layers import CustomDropout
+from models.vgg11 import VGG11Encoder
 
 
 class VGG11Classifier(nn.Module):
@@ -16,7 +18,44 @@ class VGG11Classifier(nn.Module):
             in_channels: Number of input channels.
             dropout_p: Dropout probability for the classifier head.
         """
-        pass
+        super().__init__()
+        self.features = VGG11Encoder()   # initialize the Conv Layers
+
+        # If the image size is different, to make sure that FC layers always get
+        # H,W = 7,7 we do average pooling: This is not there is vgg11 architectue
+
+        self.avgpool = nn.AdaptiveAvgPool2d((7,7))
+
+        self.classifier = nn.Sequential(
+                # FC1 : 512*7*7 -> 4096 with BN and Dropout
+                nn.Linear(512* 7 * 7, 4096),
+                nn.BatchNorm1d(4096),
+                nn.ReLU(4096),
+                CustomDropout(p=dropout_p),
+
+                # FC2 : 4096 -> 4096 with BN, Dropout
+                nn.Linear(4096, 4096),
+                nn.BatchNorm1d(4096),
+                nn.ReLU(4096),
+                CustomDropout(p=dropout_p),
+
+                # FC3: 4096 -> num_classes
+                nn.Linear(4096, num_classes),
+        )
+        
+        self._init_weights()
+    
+    def _init_weights(self):
+        
+        for m in self.classifier.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight, nonlinearity='relu')
+                nn.init.zeros_(m.bias)
+            elif isinstance(m, nn.BatchNorm1d):
+                nn.init.ones_(m.weight)
+                nn.init.zeros_(m.bias)
+        
+
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Forward pass for classification model.
@@ -25,5 +64,30 @@ class VGG11Classifier(nn.Module):
         Returns:
             Classification logits [B, num_classes].
         """
-        # TODO: Implement forward pass.
-        raise NotImplementedError("Implement VGG11Classifier.forward")
+                                      # (B, 3, H, W)
+        x = self.features(x)          # (B, 512, 7, 7) if input is 224x224
+        x = self.avgpool(x)           # (B, 512, 7, 7)
+        x = torch.flatten(x,1)        # (B, 512* 7* 7)
+        x = self.classifier(x)        # (B, num_classes)
+
+        return x
+
+    def get_block_outputs(self, x: torch.Tensor) -> dict:
+        """
+        Returns block-wise outputs in a dictionary
+        """
+
+        out = {}
+        feat = self.features
+
+        x = feat.block1(x);  out["block1"] = x
+        x = feat.block2(x);  out["block2"] = x
+        x = feat.block3(x);  out["block3"] = x
+        x = feat.block4(x);  out["block4"] = x
+        x = feat.block5(x);  out["block5"] = x
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        out["logits"] = x
+        return out
--- models/vgg11.py
+++ models/vgg11.py
@@ -6,6 +6,16 @@ from typing import Dict, Tuple, Union
 import torch
 import torch.nn as nn
 
+def _conv_bn_relu(ch_in, ch_out, kernel_size = 3, padding=1):
+    """3 x 3 block with Conv --> BatchNorm2d --> ReLU"""    
+
+    return nn.Sequential(
+        nn.Conv2d(ch_in, ch_out, kernel_size=kernel_size, padding=padding, 
+                  bias=False),
+        nn.BatchNorm2d(ch_out),
+        nn.ReLU(inplace=True),
+    )
+
 
 class VGG11Encoder(nn.Module):
     """VGG11-style encoder with optional intermediate feature returns.
@@ -13,7 +23,53 @@ class VGG11Encoder(nn.Module):
 
     def __init__(self, in_channels: int = 3):
         """Initialize the VGG11Encoder model."""
-        pass
+        super().__init__()
+
+        self.block1 = nn.Sequential(
+            _conv_bn_relu(3, 64),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+            )
+        
+        self.block2 = nn.Sequential(
+            _conv_bn_relu(64, 128),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.block3 = nn.Sequential(
+            _conv_bn_relu(128, 256),
+            _conv_bn_relu(256, 256),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+        self.block4 = nn.Sequential(
+            _conv_bn_relu(256, 512),
+            _conv_bn_relu(512, 512),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+        self.block5 = nn.Sequential(
+            _conv_bn_relu(512, 512),
+            _conv_bn_relu(512, 512),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+        self._init_weights()
+    
+    def _init_weights(self):
+        """
+        Do He/Kaiming Initialization for conv layers
+        and (mu, sigma) = (1,0) for BN layers
+        """
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out",
+                                        nonlinearity="relu")
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.ones_(m.weight)
+                nn.init.zeros_(m.bias)
+
 
     def forward(
         self, x: torch.Tensor, return_features: bool = False
@@ -28,5 +84,12 @@ class VGG11Encoder(nn.Module):
             - if return_features=False: bottleneck feature tensor.
             - if return_features=True: (bottleneck, feature_dict).
         """
-        # TODO: Implement forward pass.
-        raise NotImplementedError("Implement VGG11Encoder.forward")
+        # TODO: skip connections will be implemented later
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.block5(x)
+
+        return x
+        
