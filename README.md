85fb23bf01982c2601606913666dd9289fd16d41
Mahadevu Sudheer <mahadevusudheer274@gmail.com>
2026-04-12 21:28:52 +0530

    change unet architecture - use prepool skips

--- README.md
+++ README.md
@@ -1,46 +1,11 @@
-# DA6401 Assignment-2 Skeleton Guide
+# Complete Visual Perception Pipeline
 
-This repository is an instructional skeleton for building the complete visual perception pipeline on Oxford-IIIT Pet.
+This project involves creating a multi-task model that does classification, localization and segmentaiton using VGG11 backbone and training it from scratch on Oxford-IIIT Pets dataset.
 
+The link to the Github Repository is: [Repo](https://github.com/Sudheer-Mahadevu/complete-visual-perception-pipeline) <br>
+Following is the link for experimentation done during training the model. The results are summarised in this W&B Report: [Report](https://wandb.ai/me21b102-assign1/da6401-a2/reports/Complete-Visual-Perception-Pipeline-Report--VmlldzoxNjQ5Nzg4OA?accessToken=i9y3conlimn70gurjklbazkhmwieph62ma8m3b2b3lpzsrg7p3istqjw7aopd1sj)
 
-### ADDITIONAL INSTRUCTIONS FOR ASSIGNMENT2:
-- Ensure VGG11 is implemented according to the official paper(https://arxiv.org/abs/1409.1556). The only difference being injecting BatchNorm and CustomDropout layers is your design choice.
-- Train all the networks on normalized images as input (as the test set given by autograder will be normalized images).
-- The output of Localization model = [x_center, y_center, width, height] all these numbers are with respect to image coordinates, in pixel space (not normalized)
-- Train the object localization network with the following loss function: MSE + custom_IOU_loss.
-- Make sure the custom_IOU loss is in range: [0,1]
-- In the custom IOU loss, you have to implement all the two reduction types: ["mean", "sum"] and the default reduction type should be "mean". You may include any other reduction type as well, which will help your network learn better.
-- multitask.py shd load the saved checkpoints (classifier.pth, localizer.pth, unet.pth), initialize the shared backbone and heads with these trained weights and do prediction.
-- Keep paths as relative paths for loading in multitask.py
-- Assume input image size is fixed according to vgg11 paper(can be hardcoded need not pass as args)
-- Stick to the arguments of the functions and classes given in the github repo, if you include any additional arguments make sure they always have some default value.
-- Do not import any other python packages apart from the ones mentioned in assignment pdf, if you do so the autograder will instantly crash and your submission will not be evaluated.
-- The following classes will be used by autograder: 
-    ```
-        from models.vgg11 import VGG11
-        from models.layers import CustomDropout
-        from losses.iou_loss import IoULoss
-        from multitask import MultiTaskPerceptionModel
-    ```
-- The submission link for this assignment will be available by Saturday(04/04/2026) on gradescope
+ 
+### Note
+The models are training using ImageNet Wieights in the first satge to check if the code written and the setup are working. Once, code is verified, the models are trained **from scratch** using Xavier initialization and the same models are submitted for evaluation in GradeScope.
 
-
-
-
-
-### GENERAL INSTRUCTIONS:
-- From this assignment onwards, if we find any wandb report which is private/inaccessible while grading, there wont be any second chance, that submission will be marked 0 for wandb marks.
-- The entireity of plots presented in the wandb report should be interactive and logged in the wandb project. Any screenshot or images of plots will straightly be marked 0 for that question.
-- Gradescope offers an option to activate whichever submission you want to, and that submission will be used for evaluation. Under any circumstances, no requests to be raised to TAs to activate any of your prior submissions. It is the student's responsibility to do so(if required) before submission deadline.
-- Assignment2 discussion forum has been opened on moodle for any doubt clarification/discussion.   
-
-
-
-
-## Contact
-
-For questions or issues, please contact the teaching staff or post on the course forum.
-
----
-
-Good luck with your implementation!
--- models/segmentation.py
+++ models/segmentation.py
@@ -72,25 +72,29 @@ class VGG11UNetDecoder(nn.Module):
         """
         Channel Layout (in ---> skip ---> out)
         d5 : 512 ---> 512 ---> 256 (upsample from 7 to 14, skip from b4 pre-pool)
-        d4 : 256 ---> 256 ---> 128 (upsample from 14 to 28, skip from b3 pre-pool)
-        d3 : 128 ---> 128 ---> 64 (upsample from 28 to 56, skip from b2 pre-pool)
-        d2 : 64 ---> 64 ---> 64 (upsample from 56 to 112, skip from b1 pre-pool)
-        d1: 64 ---> 0 ---> 32 (upsample from 112 to 224, no encoder skip here)
+        d4 : 256 ---> 512 ---> 128 (upsample from 14 to 28, skip from b3 pre-pool)
+        d3 : 128 ---> 256 ---> 128 (upsample from 28 to 56, skip from b2 pre-pool)
+        d2 : 128 ---> 128 ---> 64 (upsample from 56 to 112, skip from b1 pre-pool)
+        d1: 64 ---> 64 ---> 32 (upsample from 112 to 224, no encoder skip here)
         """
 
+        # At d3, reducing the skips to 64 and continuing is another option.
+        # It reduces number of parameters and can prevent overfitting
         self.dec5 = DecoderBlock(512, 512, 256)
-        self.dec4 = DecoderBlock(256, 256, 128)
-        self.dec3 = DecoderBlock(128, 128, 64)
-        self.dec2 = DecoderBlock(64, 64, 64)
+        self.dec4 = DecoderBlock(256, 512, 128)
+        self.dec3 = DecoderBlock(128, 256, 128)
+        self.dec2 = DecoderBlock(128, 128, 64)
+        self.dec1 = DecoderBlock(64, 64, 32)
+        self.final_conv = nn.Conv2d(32, num_classes, kernel_size=1) #1x1 Projection
 
         # Final Upsample (d1) without skip connections
-        self.d1_ups = nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2)
-        self.d1_conv = nn.Sequential(
-            nn.Conv2d(32, 32, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(32),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(32, num_classes, kernel_size=1) # 1x1 projection
-        )
+        # self.d1_ups = nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2)
+        # self.d1_conv = nn.Sequential(
+        #     nn.Conv2d(32, 32, kernel_size=3, padding=1, bias=False),
+        #     nn.BatchNorm2d(32),
+        #     nn.ReLU(inplace=True),
+        #     nn.Conv2d(32, num_classes, kernel_size=1) # 1x1 projection
+        # )
 
         # do init
         self._init_weights()
@@ -108,23 +112,23 @@ class VGG11UNetDecoder(nn.Module):
     
     def forward(self, bottleneck, skips):
         """
-        skips[0] : (B,  64, 112, 112)
-        skips[1] : (B, 128,  56,  56)
-        skips[2] : (B, 256,  28,  28)
-        skips[3] : (B, 512,  14,  14)
-        bottleneck: (B, 512,   7,   7)
+        skips[0] : (B,  64, 224, 224)
+        skips[1] : (B, 128,  112,  112)
+        skips[2] : (B, 256, 56,  56)
+        skips[3] : (B, 512,  28,  28)
+        skips[4]: (B, 512,   14,   14)
+        bottleneck : (B, 512,   7,   7)
         """
 
-        s1, s2, s3, s4 = skips
+        s1, s2, s3, s4, s5 = skips
 
         # Decode
-        d = self.dec5(bottleneck, s4)   # (B, 256,  14, 14)
-        d = self.dec4(d, s3)            # (B, 128,  28, 28)
-        d = self.dec3(d, s2)            # (B,  64,  56, 56)
-        d = self.dec2(d, s1)            # (B,  64, 112, 112)
-
-        d = self.d1_ups(d)      # (B,  32, 224, 224)
-        d = self.d1_conv(d)     # (B, num_classes, 224, 224)
+        d = self.dec5(bottleneck, s5)   # (B, 256,  14, 14)
+        d = self.dec4(d, s4)            # (B, 128,  28, 28)
+        d = self.dec3(d, s3)            # (B,  64,  56, 56)
+        d = self.dec2(d, s2)            # (B,  64, 112, 112)
+        d = self.dec1(d, s1)            # (B, 32, 224, 224)
+        d = self.final_conv(d)          # (B, 3, 224, 224)
 
         return d
 
@@ -183,13 +187,14 @@ class VGG11UNet(nn.Module):
 
             return feat, pool(feat)
 
-        _, s1 = pre_pool_feat(enc.block1, x)   # s1: (B, 64,  112, 112)
-        _, s2 = pre_pool_feat(enc.block2, s1)   # s2: (B, 128,  56,  56)
-        _, s3 = pre_pool_feat(enc.block3, s2)   # s3: (B, 256,  28,  28)
-        _, s4 = pre_pool_feat(enc.block4, s3)   # s4: (B, 512,  14,  14)
-        _, bottleneck = pre_pool_feat(enc.block5, s4)   # bottleneck: (B, 512, 7, 7)
+        s1, out = pre_pool_feat(enc.block1, x)   # s1: (B, 64,  224, 224)
+        s2, out = pre_pool_feat(enc.block2, out)   # s2: (B, 128,  112,  112)
+        s3, out = pre_pool_feat(enc.block3, out)   # s3: (B, 256,  56,  56)
+        s4, out = pre_pool_feat(enc.block4, out)   # s4: (B, 512,  28,  28)
+        s5, bottleneck = pre_pool_feat(enc.block5, out)   # s5: (B, 512, 14, 14)
 
-        return [s1, s2, s3, s4], bottleneck
+        # bottleneck (B, 512, 7, 7)
+        return [s1, s2, s3, s4, s5], bottleneck
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Forward pass for segmentation model.
